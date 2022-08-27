91fda3273c8f555a88ee1b21eb2a851e722df041
nabin <nkbhaie2017@gmail.com>
2022-08-27 10:56:33 -0600

    add flask debug toolbar to the project and update the requirements.txt file to reflect the update

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 """Capstone Project 1 for web development"""
 
-from flask import Flask
+from flask import Flask, render_template
+# flask debug toolbar import
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
+#  flask debut toolbar setup
+app.config['SECRET_KEY'] = "it-is-so-secret"
+debug = DebugToolbarExtension(app)
+
+##############################################################
+
+# For set project setup, remove once everything is setup properly
 
 @app.route('/first-route')   # in terminal flask run, in browser http://127.0.0.1:5000/first-route
 def first_route():
     return "First Route for the Capstone Porject"
+
+@app.route('/first-template')
+def first_template():
+    return render_template('/first.html')
+
+
+##############################################################
